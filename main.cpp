4a74711c307d689622b817b97f3a1e528ff07aba
Archit Jain <57045366+j-archit@users.noreply.github.com>
2020-09-30 23:51:25 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-/*
+  /*
   INTELLIGENT GUIDANCE PARKING SYSTEM USING MODIFIED DIJKSTRA’S ALGORITHM
 
   Implementation of the software part of the said proposal for MTE component of
@@ -11,17 +11,18 @@
 
 #include<iostream>
 #include<climits>
+#include<vector>
 using namespace std;
 
 class parking_lots
 {
   private:
     int N;
-    int* graph;
-    int* dist;
-    int* empty;
-    int* visited;
-    int* path_prev;
+    vector<int> graph;
+    vector<int> dist;
+    vector<int> empty;
+    vector<int> visited;
+    vector<int> path_prev;
 
   public:
     // Default Constructor
@@ -32,15 +33,10 @@ class parking_lots
 
     /*
       Returns the Node with the smallest distance from source that
-      has not been visited
+      has not been visited, if no such node exists (all nodes visited)
+      then returns -1
     */
     int min_not_visited();
-
-    /*
-      Checks if all nodes visited?
-      returns 1 if not visited, 0 if all nodes have been visited
-    */
-    int not_vis();
 
     /*
       Updates Shortest distances from the given node
@@ -48,8 +44,6 @@ class parking_lots
     void update_dist(int);
 
     void print();
-
-    void delete_mem();
 };
 
 // Member Functions Definitions
@@ -58,75 +52,72 @@ parking_lots::parking_lots()
   cout << "Nodes: " ;
   cin >> N;
 
+  graph.reserve(N*N);
+  dist.reserve(N);
+  empty.reserve(N);
+  visited.reserve(N);
+  path_prev.reserve(N);
+
   // Allocate and Initialise the Graph
   cout << "Distances as an Adjacency Matrix:" << endl;
-  graph = new int[N*N];
+
   for(int i = 0 ; i < N*N; i++)
-    cin >> *(graph+i*sizeof(int));
+  {
+    int d;
+    cin >> d;
+    graph.push_back(d);
+  }
 
   // Allocate and Initialise distances array
-  dist = new int[N];
   dist[0] = 0;
   for(int i = 1; i < N; i++)
     dist[i] = INT_MAX;    // Initialise the distances to maximum possible
 
   // Allocate and Initialise lot empty array
-  empty = new int[N];
   empty[0] = -1;          // Source Node not available for parking (Entrance)
   for(int i = 1; i < N; i++)
     empty[i] = 1;
 
-  cout<<"3";
+  // Allocate and Initialise visited array
+  for(int i = 0; i < N; i++)
+    visited[i] = 0;
+  
 }
 
 void parking_lots::shortest()
 {
-  cout<<"2";
-  // Allocate and Initialise visited array
-  visited = new int[N];
-  for(int i = 0; i < N; i++)
-    visited[i] = 0;
-
   // Source (Entrance, 0) as initial node
-  int curr;
+  int curr = 0;  
   path_prev[0] = 0;
-  cout<<"1";
-  while(not_vis())
+  do
   {
-    curr = min_not_visited();
-    cout << curr;
+    //cout << curr;
     update_dist(curr);
     visited[curr] = 1;
+    curr = min_not_visited();
 
-    cout<<"heree";
-  }
+  } while(curr != -1);
 }
 
 int parking_lots::min_not_visited()
 {
-  float* dist_n = new float[N];
+  vector<int> dist_n;
+  dist_n.reserve(N);
+
   for(int i = 0; i < N; i++)
     dist_n[i] = dist[i]*(!visited[i]);
 
-  float min = dist_n[0];
-  int ind = 0;
+  int min = INT_MAX;
+  int ind = -1;
   for(int i = 1; i < N; i++)
   {
-    if( dist_n[i] <= min && dist_n != 0 )
+    if( dist_n[i] < min && dist_n[i] != 0 )
     {
       min = dist_n[i];
       ind = i;
     }
   }
   return ind;
-}
-
-int parking_lots::not_vis()
-{
-  for(int i = 0; i < N; i++)
-    if(visited[i] == 0)
-      return 1;
-  return 0;
 }
 
 void parking_lots::update_dist(int node)
@@ -148,7 +139,7 @@ void parking_lots::update_dist(int node)
 void parking_lots::print()
 {
   // Printing Everything
-  cout << "Total Nodes: " << N <<endl;
+  cout << "\nTotal Nodes: " << N <<endl;
 
   int k = 0;
   cout << "Graph: " << endl;
@@ -159,42 +150,34 @@ void parking_lots::print()
       k = 0;
       cout << endl;
     }
-    cout << *(graph+i*sizeof(int));
+    cout << graph[i];
     k++;
   }
 
-  cout << "\nDistances: ";
+  cout << "\nDistances:\n";
   for(int i = 0 ; i < N; i++)
     cout << dist[i] << "\t";
 
-  cout << "\nAvailable Lots: ";
+  cout << "\nAvailable Lots:\n";
   for(int i = 0 ; i < N; i++)
     cout << empty[i] << "\t";
 
-  cout << "\nVisited Nodes: ";
+  cout << "\nVisited Nodes:\n";
   for(int i = 0 ; i < N; i++)
     cout << visited[i] << "\t";
 
-  cout << "\nPath Prev: ";
+  cout << "\nPath Prev:\n";
   for(int i = 0 ; i < N; i++)
     cout << path_prev[i] << "\t";
-}
-
-void parking_lots::delete_mem()
-{
-  delete [] graph;
-  delete [] dist;
-  delete [] empty;
-  delete [] visited;
-  delete [] path_prev;
 }
 
 int main()
 {
   parking_lots lot;
-  lot.shortest();
-  lot.print();
 
-  lot.delete_mem();
+  lot.shortest();
+  cout << endl;
+  lot.print();
+  
   return 0;
 }
